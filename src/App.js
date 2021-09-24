8e0a108e0b4c8e7560785a0a62d482066f1ca30d
Jack Davey <jackdavey@Jacks-MacBook-Pro.local>
2021-09-24 01:40:07 -0700

    Hid alert bar:

--- src/App.js
+++ src/App.js
@@ -4,27 +4,23 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import AlertBar from "./components/AlertBar"
 import { Link } from "react-scroll";
-import React, {useRef} from "react";
 
 import './App.css';
 
 
 function ScrollArrow () {
-  return <div onclick={executeScroll} class="arrow"></div>;
+  return <div class="arrow"></div>;
   //Create arrow component here alongside scrolling function, import to Splash componenet and 
 }
 
-function executeScroll(){
-  //something here later
-}
+
 
 function App() {
-  const aboutRef = useRef(null)
   
 
   return (
     <div class="App">
-      <AlertBar />
+      {/*<AlertBar />*/}
       <Link to="aboutSect" spy={true} smooth={true} offset={50} duration={500}>Test</Link>
       <Splash />
       <About id="aboutSect"/>
