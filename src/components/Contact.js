ce45f9c1b52509285f59b12df4d56331790c7bad
Jack Davey <jackdavey@Jacks-MacBook-Pro.local>
2021-09-23 21:55:26 -0700

    Added contact form, still working on layout, design, and functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,53 @@
-export default function Contact(){
-  return(
-  <div class="col-sm-12 container contact-section">
+import React, { useState } from "react";
+
+const Contact = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+  return (
+<div class="col-sm-12 container contact-section">
       <div class="row">
         <h1 class="sectionHeader"> Contact Me!</h1>
       </div>
       <div class="row">
-        <div class="col-sm-12">
-          <p>This is where I'll put a PHP form to contact me (you guessed it, when I get around to that bit) so my personal info doesn't get scraped to hell by bots</p>
-        </div>
+
+    <form onSubmit={handleSubmit}>
+    <div id="formWrap">
+      <div id="nameWrap">
+        <input type="text" id="name" placeholder="Name" required />
       </div>
+      <div id="emailWrap">
+        <input type="email" id="email" placeholder="Email" required />
+      </div>
+      <div id="messageWrap">
+        <textarea id="message" placeholder="Message" required />
+      </div>
+      <button type="submit">{status}</button>
+      </div>
+    </form>
+
     </div>
-    );
-}
+    </div>
+  );
+};
+
+export default Contact;
+
