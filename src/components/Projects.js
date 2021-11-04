0f9bd3f605fe5480c4dc013019b6bb196b54ade7
jdavey@scu.edu <jdavey@scu.edu>
2021-11-03 23:44:54 -0700

    Fixed project links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,45 +8,45 @@ export default function Projects(){
         <h1 class="sectionHeader"> Projects </h1> 
       </div>
       <div class="row project-wrap">
-        {/*<a href="hubMgmtInterface/hub.html">*/}
           <div class="project-card col-sm-2">
+          <a href={"projects/HubHouseMgmt/index.html"}>
             <div class="project"> 
               <h5>House Grocery &amp; Bill Management App</h5>
-
             </div>
+            </a>
           </div>
-        {/*</a>*/}
-        {/*<a href="" onclick="return false;">*/}
         <div class="project-card col-sm-2 blocked">
+        <a href={""}>
           <div class="project"> 
             <h5>The Emergency Exit Band Website</h5>
           </div>
+          </a>
         </div>
-        {/*</a>*/}
 
-        {/*<a href="" onclick="return false;">*/}
           <div class="project-card col-sm-2 blocked">
-            <div class="project"> 
-              <h5>Some Other Project</h5>
-            </div>
+            <a href={""}>
+              <div class="project"> 
+                <h5>ReactJS Minigames</h5>
+              </div>
+            </a>
           </div>
-        {/*</a>*/}
-        {/*<a href="" onclick="return false;">*/}
+        
           <div class="project-card col-sm-2 blocked">
-            <div class="project"> 
-              <h5>And Another Project</h5>
-            </div>
+            <a href={""}>
+              <div class="project"> 
+                <h5>Some Other Project</h5>
+              </div>
+            </a>
           </div>
-        {/*</a>*/}
-        {/*<a href="https://github.com/jackcdavey">*/}
+        
           <div class="project-card col-sm-2 blocked">
-            <div class="project">
+          <a href={"https://github.com/jackcdavey"}>
+            <div class="project" id="gitHubCard">
               <img src={gitLogo} id="githubimg"class="img-fluid rounded center-block" alt="GitHub Logo"></img>
-              <h5> GitHub </h5>
+              <h5 > GitHub </h5>
             </div>
-
+        </a>
           </div>
-        {/*</a>*/}
       </div>
     </div>
     );
