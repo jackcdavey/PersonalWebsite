d076b5d940d902ef9341d788ce92d77f5d32ed90
jackcdavey <jdavey@scu.edu>
2022-04-17 02:09:56 -0700

    Integrated styled components

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default function About() {
+    return (
+        <p>Currently pursuing a B.S. in Web Design and Engineering with a minor in
+            Innovation, Design Thinking, and the Entrepreneurial Mindset at Santa Clara University.
+            Having spent my entire life in California, I grew up with an immense appreciation for
+            the beauty and simplicity of nature. I love to tinker, design, and create, but when I’m
+            not working away at my desk, you’ll likely find me at the driving range or exploring
+            the mountains with my hammock and camera. </p>
+    );
+}
---
+++ src/styles/stylesheet.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+export const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
