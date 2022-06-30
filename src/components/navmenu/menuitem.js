f1b1442e9f9afb8358421fccccfdf68a1411a5b2
Jack Davey <jackdavey@Jacks-MacBook-Pro.local>
2022-06-29 17:25:23 -0700

    Fixed navbar click response issue, still working on automatic close

--- src/components/navmenu/menuitem.js
+++ src/components/navmenu/menuitem.js
@@ -26,7 +26,7 @@ const colors = window.matchMedia("(prefers-color-scheme:  )").matches ? ["#5d173
 export const MenuItem = ({ i, sectionLabel, sectionTarget, forwardedRef }) => {
 	const style = { color: `${colors[i]}` }
 	return (
-		<motion.li  variants={variants} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }} ref={forwardedRef}>
+		<motion.li  variants={variants} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} ref={forwardedRef}>
 			<Link className="nav-link" activeClass="active" to={sectionTarget} spy={true} smooth={true} duration={700} style={{ zIndex: 10, display: "flex", alignItems: "center" }}>
 				<div style={style}>
 					<TaglineTxt>{sectionLabel}</TaglineTxt>
