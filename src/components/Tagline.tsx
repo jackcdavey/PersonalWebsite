63cc958ee0137e14af06d10bcd3fbe916bad2a9d
jackcdavey <johncdavey2000@gmail.com>
2023-12-28 15:26:41 -0800

    Updated tagline to display words in a random order

--- src/components/Tagline.tsx
+++ src/components/Tagline.tsx
@@ -1,11 +1,26 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
 import { TaglineTxt } from "../styles/stylesheet"
 
-const words = ["Developer", "Designer", "Tinkerer", "Photographer", "Audiophile", "Ordained Minister", "Pinewood Derby State Champ (2007-09)", "Plant Dad", "Sudoku Enthusiast"]
+// const words = ["Developer", "Designer", "Tinkerer", "Photographer", "Audiophile", "Ordained Minister", "Pinewood Derby State Champ (2007-09)", "Plant Dad", "Sudoku Enthusiast"]
+
+// Separated into two arrays to ensure that the initial words are always shown first, then the other words are shuffled
+const initialWords = ["Designer", "Developer", "Tinkerer"];
+const otherWords = ["Photographer", "Audiophile", "Ordained Minister", "Pinewood Derby State Champ (2007-09)", "Plant Dad", "Sudoku Enthusiast"];
+
+
+const shuffleArray = (array: string[]) => {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+}
 
 export default function Tagline() {
-	const [index, setIndex] = React.useState(0)
+	const [index, setIndex] = useState(0)
+	const [shuffledWords, setShuffledWords] = useState([...initialWords, ...shuffleArray(otherWords)])
+
 	useEffect(() => {
 		const intervalId = setInterval(() => setIndex((index) => index + 1), 2000)
 		return () => {
@@ -22,8 +37,8 @@ export default function Tagline() {
 				exit={{ opacity: 0, y: -20 }}
 				transition={{ ease: "easeInOut", duration: 0.5 }}
 			>
-				<TaglineTxt >{words[index % words.length]}</TaglineTxt>
+				<TaglineTxt>{shuffledWords[index % shuffledWords.length]}</TaglineTxt>
 			</motion.div>
-		</AnimatePresence >
+		</AnimatePresence>
 	)
 }
