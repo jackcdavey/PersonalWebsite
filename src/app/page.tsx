68875f8d9b560a2a33e3bd1039ea891d81d77037
jackcdavey <johncdavey2000@gmail.com>
2024-01-10 16:01:33 -0800

    Implemented splash scroll arrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from 'next/image'
+import { Element } from 'react-scroll'
 import styles from './styles/page.module.css'
 import AboutMe from './sections/aboutMe'
 import Splash from './sections/splash'
@@ -10,10 +13,18 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <FramerBG />
-      <Splash />
-      <AboutMe />
-      <Projects />
-      <Contact />
+      <Element name="splash">
+        <Splash />
+      </Element>
+      <Element name="aboutme">
+        <AboutMe />
+      </Element>
+      <Element name="projects">
+        <Projects />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
     </main>
   )
 }
