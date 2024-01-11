f86f7f2b0cb6d2a9213822011eab5accbb5c41a3
jackcdavey <johncdavey2000@gmail.com>
2024-01-10 16:25:47 -0800

    Implemented dynamic import to fix build on Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,21 @@
 
 import Image from 'next/image'
 import { Element } from 'react-scroll'
+import dynamic from 'next/dynamic'
 import styles from './styles/page.module.css'
-import AboutMe from './sections/aboutMe'
-import Splash from './sections/splash'
-import Projects from './sections/projects'
-import Contact from './sections/contact'
-import FramerBG from './components/framerBG'
+// import AboutMe from './sections/aboutMe'
+// import Splash from './sections/splash'
+// import Projects from './sections/projects'
+// import Contact from './sections/contact'
+// import FramerBG from './components/framerBG'
+
+const FramerBG = dynamic(() => import('./components/framerBG'), { ssr: false })
+const AboutMe = dynamic(() => import('./sections/aboutMe'), { ssr: false })
+const Splash = dynamic(() => import('./sections/splash'), { ssr: false })
+const Projects = dynamic(() => import('./sections/projects'), { ssr: false })
+const Contact = dynamic(() => import('./sections/contact'), { ssr: false })
+
+
 
 export default function Home() {
   return (
