1dc9b073416142b3cccd551bd3662e8582b940c6
jackcdavey <johncdavey2000@gmail.com>
2024-01-11 21:24:25 -0800

    Enabled Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 import './globals.css'
 
 
@@ -20,6 +21,15 @@ export default function RootLayout({
         {children}
         <SpeedInsights />
         <Analytics />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-MR3WRB99PZ" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'G-MR3WRB99PZ');
+          `}
+        </Script>
       </body>
     </html>
   )
